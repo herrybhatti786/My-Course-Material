61451c1a1e6b0727346e1c14371ea55bbab2aac2
Haris Riaz <herrybhatti786@gmail.com>
2026-01-19 23:38:13 +0500

    css and java script from start

---
+++ New folder/Assignment 7 (web page)/javascript chapter/app.js
@@ -0,0 +1,85 @@
+// var myName = "Haris";
+// var myAge = 31;
+// var myBoolean = true;
+// var myNull = null;
+// var myUndefined = undefined;
+// console.log("My Name is " + myName);
+// console.log("My Age is " + myAge);
+// console.log("My Boolean is " + myBoolean);
+// console.log("My Null is " + myNull);
+// console.log("My Undefined is " + myUndefined);
+
+// var firstName = "Haris";
+// var lastName = "Ahmed";
+// var fullName = firstName + " " + lastName;
+// console.log("Full Name: " + fullName);
+
+// var yourName = "Haris Riaz";
+// var yourPass = "1234bhatti";
+// var isName = prompt("Enter your name");
+// var isPass = prompt("Enter your password");
+
+// if (isName === "Haris Riaz" && isPass === "1234bhatti") {
+//     alert("login Successfull");
+// } else {
+//     alert("invalid credentials");
+// }
+
+// var Grade = prompt("Enter your Marks");
+
+// if(Grade >= 90) {
+//     console.log('your Grade is "A+1"');
+// } else if(Grade >= 80) {
+//     console.log('Ypur Grade is "A1"');
+// } else if(Grade >= 70) {
+//     console.log('Your Grade is "A"');
+// } else if(Grade >= 60) {
+//     console.log('Your Grade is "B"');
+// } else if(Grade >= 50) {
+//     console.log('Your Grade is "C"');
+// } else if(Grade >= 40) {
+//     console.log('Your Grade is "D"');
+// } else {
+//     console.log('You are "Fail"');
+// }
+
+
+// var userName = "Haris Riaz";
+// var userPassword = "123bhatti";
+// var userActive = true;
+
+// if(prompt("Enter yout username") === userName) {
+//     if(prompt("Enter your password") === userPassword) {
+//                 alert("login Successfull");
+//         } 
+//      } else {
+//             alert("Invalid Password");
+//             }
+//          else {
+//             alert("Invalid Username");
+//      }
+
+
+// const students = [
+//    "Haris",
+//    "Abdullah",
+//    "Basit"
+// ]
+// students.unshift(prompt("Enter your name"));
+// console.log(students);
+// alert(students);
+
+// let students = [
+//    "Haris",
+//    "Abdullah",
+//    "Basit",
+//    "Hashir",
+//    "Moosa",
+//    "Dawood",
+//    "Asad",
+//    "Ibraheem"
+// ]
+
+// slicedStudents = students.slice(2,5,);
+// console.log(students, slicedStudents);
+
